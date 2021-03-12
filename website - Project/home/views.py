c01f2f3be30e6c5a07871d4f5e357537f03658fa
Preyash Patel <preyash2047@GMAIL.com>
2021-03-12 20:05:35 +0530

    #live

--- website - Project/home/views.py
+++ website - Project/home/views.py
@@ -9,6 +9,201 @@ import yfinance as yf
 from .forms import DataModelForm
 from .models import DataModel
 
+from pandas_datareader import data, wb
+import pandas as pd
+import numpy as np
+import datetime
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+
+
+def predict_stock_price(start_year, start_month, start_day, code):
+    start = datetime.datetime(start_year, start_month, start_day)
+    end = datetime.date.today()
+    stockData = data.DataReader(code, 'yahoo', start, end)
+
+    def get_rsi(sdata, m, mem):
+        neg = 0
+        pos = 0
+        RS = 0
+        RSI = 100
+
+        upcloses = 0
+        downcloses = 0
+
+        n = m
+        k = m - 1
+
+        for p in range(mem):
+            diff = sdata[n, 3] - sdata[k, 3]
+            if (diff >= 0):
+                upcloses = upcloses + diff
+                pos = pos + 1
+            else:
+                downcloses = downcloses + diff
+                neg = neg + 1
+
+            n = n - 1
+            k = k - 1
+
+        downcloses = -downcloses
+        if (neg == 0):
+            return 100
+        else:
+            RS = (upcloses * neg) / (downcloses * pos)
+
+        RSI = 100 - (100 / (1 + RS))
+
+        return RSI
+
+    def get_mfi(sdata, m, mem):
+        neg = 0
+        pos = 0
+        MFR = 0
+        MFI = 100
+
+        pmflow = 0
+        nmflow = 0
+
+        n = m
+        k = m - 1
+
+        for p in range(mem):
+
+            typ_pricec = (sdata[n, 0] + sdata[n, 1] + sdata[n, 3]) / 3
+            typ_pricep = (sdata[k, 0] + sdata[k, 1] + sdata[k, 3]) / 3
+
+            # print(typ_price,sdata[n,0],sdata[n,1],sdata[n,3])
+
+            if (typ_pricec >= typ_pricep):
+                pmflow = pmflow + ((sdata[n, 4]) * (typ_pricec))
+                pos = pos + 1
+            else:
+                nmflow = nmflow + ((sdata[n, 4]) * (typ_pricec))
+                neg = neg + 1
+
+            n = n - 1
+            k = k - 1
+
+        if (neg == 0):
+            return 100
+        else:
+            MFR = pmflow / nmflow
+
+        MFI = 100 - (100 / (1 + MFR))
+
+        return MFI
+
+    def get_ema(sdata, m, mem, EMAp):
+
+        EMA = sdata[m, 3] * (2 / (1 + mem)) + (1 - (2 / (1 + mem))) * EMAp
+        # print("EMA",EMA)
+
+        return EMA
+
+    def get_so(sdata, m, mem):
+
+        SO = ((sdata[m, 3] - sdata[m, 1]) / (sdata[m, 0] - sdata[m, 1])) * 100
+        # print("SO",SO)
+        return SO
+
+    memory = 14
+    sdat2 = stockData.reset_index()
+    del sdat2["Date"]
+    del sdat2["Adj Close"]
+    sdat3 = np.array(sdat2, dtype=np.float32)
+
+    # print(sdat3[0])
+
+    df1 = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Volume', 'RSI', 'MFI', 'EMA', 'SO', 'CloseNext'])
+
+    df2 = pd.DataFrame(columns=['Close', 'RSI', 'MFI', 'EMA', 'SO', 'CloseNext'])
+
+    arr = np.array(df1.values)
+
+    # print("Printing j\n")
+
+    EMAp = 0
+    acc = 0
+
+    for i in range(memory):
+        acc = acc + sdat3[i, 3]
+
+    EMAp = acc / memory
+
+    # for i in range(len(sdat3) -memory):
+    for i in range(len(sdat3) - 1 - memory):
+        j = i + memory
+
+        RSI = get_rsi(sdat3, j, memory)
+        # print("RSI:",RSI)
+
+        MFI = get_mfi(sdat3, j, memory)
+        # print("MFI:",MFI)
+
+        EMA = get_ema(sdat3, j, memory, EMAp)
+        EMAp = EMA
+
+        SO = get_so(sdat3, j, memory)
+
+        N_close = sdat3[j + 1, 3]
+
+        rec1 = [sdat3[j, 2], sdat3[j, 0], sdat3[j, 1], sdat3[j, 3], sdat3[j, 4], RSI, MFI, EMA, SO, N_close]
+        rec2 = [sdat3[j, 3], RSI, MFI, EMA, SO, N_close]
+
+        if (sdat3[j, 4] != 0):
+            d1 = {"Open": sdat3[j, 2], "High": sdat3[j, 0], "Low": sdat3[j, 1], "Close": sdat3[j, 3],
+                  "Volume": sdat3[j, 4], "RSI": RSI, "MFI": MFI, "EMA": EMA, "SO": SO, "CloseNext": N_close}
+            df1.loc[i] = rec1
+            df2.loc[i] = rec2
+
+    # ANN
+    dataset = df1
+    y = pd.DataFrame(dataset['CloseNext'])
+    X = dataset.drop(['CloseNext'], axis=1)
+
+    X = np.array(X)
+    y = np.array(y)
+    # X = X[1700:2030,:]
+    # y = y[1700:2030,:]
+    y = y.flatten()
+
+    # Feature scaling
+    scaled = StandardScaler()
+    scaled.fit(X)
+    X = scaled.transform(X)
+
+    # Train Test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+    ## ANN implementation
+    # define base model of our neural network for regression taks
+    def endgame():
+        # Adding the neurons in various layers
+        model = Sequential()
+        model.add(Dense(9, input_dim=9, kernel_initializer='normal', activation='relu'))
+        model.add(Dense(15, kernel_initializer='normal', activation='relu'))
+        model.add(Dense(8, kernel_initializer='normal', activation='relu'))
+        model.add(Dense(1, kernel_initializer='normal'))
+        # Compile model for our use in KerasRegressor
+        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mape'])
+        return model
+
+    ann_regression = KerasRegressor(build_fn=endgame, epochs=100, batch_size=5, verbose=1)
+
+    ann_regression.fit(X_train, y_train)
+
+    ann_predict = ann_regression.predict(X_test)
+
+    error = mean_absolute_error(ann_predict, y_test)
+    per_err = (error / np.mean(y_test)) * 100
+    print('The mean absolute error is {} and percentage error is {}.'.format(error, per_err))
+
+    return ann_regression.predict(scaled.transform([df1.iloc[-1, :-1].values.tolist()]))
 
 # Create your views here.
 def home(request):
@@ -124,4 +319,14 @@ def holdings(request):
     return render(request, "home/holdings.html", {"error": "", "form": "", "holding_stock": holding_stock})
 
 def predictions(request):
-    return render(request, "home/predictions.html", {"error": "predictions pending"})
+    if request.method == "GET":
+        return render(request, "home/predictions.html")
+    else:
+        # parameter
+        start_year = 2011
+        start_month = 1
+        start_day = 1
+        code = request.POST["symbol"]
+        predicted_price = predict_stock_price(start_year, start_month, start_day, code)
+        predicted_price = int(predicted_price)
+        return render(request, "home/predictions.html", {"error": "", "predicted_price":predicted_price})
